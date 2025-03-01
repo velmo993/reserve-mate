839091d291b1bf21fe5c86ecc56d0bba640d14c2
velmo993 <velmovszkib@gmail.com>
2025-03-01 15:41:32 +0100

    date-range-form, date-time-form css fix

--- reserve-mate.php
+++ reserve-mate.php
@@ -3,7 +3,7 @@
 Plugin Name: Reserve Mate
 Plugin URI: https://github.com/velmo993/reserve-mate
 Description: A plugin for managing reservations.
-Version: 1.0.2
+Version: 1.0.3
 Author: velmoweb.com
 Author URI: https://example.com
 License: GPL2
